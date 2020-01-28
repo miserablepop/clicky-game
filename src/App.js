048ce42b5599fef052e8f3e16b291b8856ae7fbf
JC Lenz <jc@miserablepop.com>
2020-01-27 20:25:44 -0800

    testing relative path bug for cards.json

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ class App extends Component {
 
  render() {
   return (
-    <Router>
+    <Router basename="/clicky-game">
       <div className="Site">
         <Header 
           total={this.state.score}
