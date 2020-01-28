59964e2dff396c7d6a57c4eab4d361047a22bcb9
JC Lenz <jc@miserablepop.com>
2020-01-27 18:23:38 -0800

    Updates

---
+++ precache-manifest.d9f8846cce022feb72d576884c5bf270.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67e4b0b20fb345396dafc64fa230aa21",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "b58120e5b636f82de260",
+    "url": "/clicky-game/static/css/main.16c87c5c.chunk.css"
+  },
+  {
+    "revision": "3d12d491712c7091636c",
+    "url": "/clicky-game/static/js/2.f28a6324.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/clicky-game/static/js/2.f28a6324.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b58120e5b636f82de260",
+    "url": "/clicky-game/static/js/main.d3ec3dc5.chunk.js"
+  },
+  {
+    "revision": "bad51917f4d1f3d0e133",
+    "url": "/clicky-game/static/js/runtime-main.fe7d041e.js"
+  }
+]);
